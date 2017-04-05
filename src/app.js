89223c2205acc22fd14132384db6e5f7ab58544e
mcastre <mcastre3@gmail.com>
2017-04-05 13:20:34 -0500

    Add contentful entry point

--- src/app.js
+++ src/app.js
@@ -16,6 +16,13 @@ import './stylesheets/app.css';
 // VENDOR JS SCRIPTS ----------------------------
 
 import './vendor/ui-bootstrap-tpls-2.2.0.min.js';
+import './vendor/angular-contentful.min.js';
 
-angular.module('app', [uirouter, home])
+angular.module('app', [uirouter, home, 'contentful'])
+  .config(function(contentfulProvider) {
+    contentfulProvider.setOptions({
+      space: 'jvijs8gputds',
+      accessToken: '0cbeef691191860f77f0f682fa50ef9a9aa76f008a7349b6fb7a59a4914ae653'
+    });
+  })
   .config(routing);
